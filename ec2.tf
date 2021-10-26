c868b4a212a94354d6f18d87b1c31ac4a6c543ee
sharad <sharad@gmail.com>
2021-10-26 15:27:11 +0530

    frist commit

---
+++ ec2.tf
@@ -0,0 +1,44 @@
+ec2.tf
+
+provider  "aws" {
+  access_key = "AKIAYFLGAMTYJ2NHNWDN"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+resource "aws_instance" "Jenkins_server" {
+  ami           = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+  key_name  = "Jenkins_Instance"
+  count  = "1"
+  security_groups = ["security_jenkins_port"]
+  tags = {
+   name = "jenkins_Instance"
+ }
+}
+
+resource "aws_security_group" "security_jenkins_port" {
+  name        = "security_jenkins_port"
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
